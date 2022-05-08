c5986d12c74e5642e35a929ab6bd2a9457c94ed2
men21049 <men21049@byui.edu>
2022-05-07 22:10:15 -0500

    initial set up

--- cse210-week3/cards.py
+++ cse210-week3/cards.py
@@ -1,1 +1,11 @@
 import random
+
+
+class cards:
+
+    def __init__(self):
+        self.card_list = [i + 1 for i in range(13)]
+
+    def get_random_card(self):
+        random_number = random.choice(self.card_list)
+        return random_number
